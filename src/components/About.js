d8545f81137fe36a3acf2907bc377039fa7a992c
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2024-01-11 23:01:40 +0530

    Add about section and profile  url section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,36 @@
-const About=()=>{
-    return(
-        <div>
-            <h1> hello i am sandeep kumar sahoo</h1>
-        </div>
-    )
+import React from "react";
+import { Outlet, Link  } from "react-router-dom";
+class About extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log('parent constructor')
+        this.state = {
+            profile: {
+                show: false
+            }
+        }
+    }
+
+    handleToggleProfile = () => {
+        this.setState((prevState) => ({
+            profile: {
+                show: !prevState.profile.show
+            }
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1><b>About Us Page</b></h1>
+                <h4> hello i am sandeep kumar sahoo</h4>
+                <Link to='/about/profile'>
+                    <button onClick={this.handleToggleProfile}>{this.state.profile.show ? 'Go to About' : 'Go to Profile'}</button>
+                </Link>
+                <Outlet />
+            </div>
+        )
+    }
 }
 
 export default About;
