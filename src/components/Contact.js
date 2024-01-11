e89dfac641554312b6e22942c32a4bb48178f1f3
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2024-01-11 18:20:14 +0530

    Add Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+// import ShimmerCard from "./Shimmer"
+
+const Contact = () => {
+    return  (
+        <div>
+            <h1> Never Hesitate to contatct me </h1>
+        </div>
+    )
+}
+export default Contact
